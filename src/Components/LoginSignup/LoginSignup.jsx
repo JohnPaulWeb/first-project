d20ec493548cc059fd1cbc366a182e7db4f315bf
Ayanokoji <142859609+JohnPaulWeb@users.noreply.github.com>
2024-07-02 12:42:35 +0800

    Add files via upload

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import '.LoginSignup.css'
+const LoginSignup = () => {
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignup
